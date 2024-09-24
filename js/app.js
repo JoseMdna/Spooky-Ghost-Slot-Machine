54cba37e8970b6fd5e3777382bab4d114486446d
JoseMdna <jose73001@gmail.com>
2024-09-24 12:32:51 -0700

    organizing js page, adding my cached elementsand empty functions

--- js/app.js
+++ js/app.js
@@ -7,14 +7,36 @@ const icons = []
 let reel1 = null
 let reel2 = null
 let reel3 = null
+
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const spinButton = document.getElementById('spin-button')
+const reel1eElement = document.getElementById('reel1')
+const reel2eElement = document.getElementById('reel2')  
+const reel3eElement = document.getElementById('reel3')
+const messageElement = document.getElementById('message')
 
 /*-------------------------------- Functions --------------------------------*/
+const spinReels = () => {
+    // Function logic here
+}
 
+const getRandomIcon = () => {
+  // Function logic here
+}
+
+const updateReels = () => {
+  // Function logic here
+}
+
+const checkWinner = () => {
+    // Function logic here
+    }
+
+const checkForJackpot = () => {
+    // Function logic here
+}
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+spinButton.addEventListener('click', spinReels)
